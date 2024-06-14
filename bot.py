cf95a014c38cc9c1992871f4d9b22bf8b9dbff57
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-06-14 10:35:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,66 @@
-import os
-
-from dotenv import load_dotenv
-from telegram.ext import Updater, MessageHandler, Filters
 
 
-def echo(update, context):
+import logging
+
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Hi {user.mention_markdown_v2()}\!',
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+def love_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Love!')
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
     update.message.reply_text(update.message.text)
 
 
-if __name__ == '__main__':
-    load_dotenv()
-    tg_token = os.getenv("TELEGRAM_BOT_TOKEN")
-    updater = Updater(tg_token)
+def main() -> None:
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater('6593398730:AAHwO9GX8AIArqLvBp1_q-5OGOsMuO4nt0M')
+
+    # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(MessageHandler(Filters.text, echo))
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("love", love_command))
+
+    # on non command i.e message - echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Start the Bot
     updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
+
+
+if __name__ == '__main__':
+    main()
